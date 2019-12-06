977eb7f8dc4b5f8d7f73c84500afb4f8ba7f3686
hongker <xiaok2013@live.com>
2019-12-06 17:11:24 +0800

    增加mysql连接组

--- component/mysql/mysql.go
+++ component/mysql/mysql.go
@@ -4,8 +4,8 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"sync"
-	"fmt"
 	"os"
+	"fmt"
 )
 
 const (
@@ -13,8 +13,33 @@ const (
 	defaultOpenConnections = 40
 )
 
+var group *ConnectionGroup
+
+func init()  {
+	group = &ConnectionGroup{
+		lock: &sync.Mutex{},
+		connections: make(map[string]*gorm.DB),
+	}
+}
+
+// ConnectionGroup 数据库连接组
+type ConnectionGroup struct {
+	lock *sync.Mutex
+	defaultName string
+	connections map[string]*gorm.DB
+}
+
 // Conf 数据库配置
 type Conf struct {
+	// 数据库名称
+	Name string
+
+	// 是否为默认连接
+	Default bool
+
+	// 连接失败的处理方式
+	ConnectFailedHandler func(err error)
+
 	// dsn
 	Dsn string
 
@@ -28,8 +53,17 @@ type Conf struct {
 	MaxOpenConnections int
 }
 
-var dbInitOnce sync.Once
-var connection *gorm.DB
+func CloseConnectionGroup()  {
+	if len(group.connections) == 0 {
+		return
+	}
+
+	for _, conn := range group.connections {
+		conn.Close()
+	}
+
+	return
+}
 
 // complete 补全
 func (conf *Conf) complete() {
@@ -40,33 +74,54 @@ func (conf *Conf) complete() {
 	if conf.MaxOpenConnections == 0 {
 		conf.MaxOpenConnections = defaultOpenConnections
 	}
+
+	if conf.ConnectFailedHandler == nil {
+		conf.ConnectFailedHandler = func(err error) {
+			fmt.Printf("database %s connect failed:%s", conf.Name, err.Error())
+			os.Exit(-1)
+		}
+	}
 }
 
 // 初始化数据库连接池
-func InitPool(conf Conf) (err error) {
+func InitPool(confItems ...Conf) (err error) {
+	group.lock.Lock()
 
-	dbInitOnce.Do(func() {
-		conf.complete()
-
-		client, err := gorm.Open("mysql", conf.Dsn)
-		if err != nil {
-			fmt.Println("数据库连接失败:", err)
-			os.Exit(-1)
+	defaultConnectionName := ""
+	for key, conf := range confItems {
+		// 如果没有设置default选项，则默认取第一个
+		if key == 0 {
+			defaultConnectionName = conf.Name
+		}else if conf.Default {
+			defaultConnectionName = conf.Name
 		}
 
-		connection = client
+		conf.complete()
+
+		connection, err := gorm.Open("mysql", conf.Dsn)
+		if err != nil {
+			conf.ConnectFailedHandler(err)
+		}
 
 		// 设置是否打印日志
 		connection.LogMode(conf.LogMode)
 		// 设置连接池
 		connection.DB().SetMaxIdleConns(conf.MaxIdleConnections)
 		connection.DB().SetMaxOpenConns(conf.MaxOpenConnections)
-	})
+
+		group.connections[conf.Name] = connection
+	}
+	group.defaultName = defaultConnectionName
 
 	return nil
 }
 
 // 获取数据库连接
 func GetConnection() *gorm.DB {
-	return connection
+	return GetConnectionByName(group.defaultName)
+}
+
+// GetConnectionByName 根据名称获取数据库连接
+func GetConnectionByName(name string) *gorm.DB {
+	return group.connections[name]
 }
