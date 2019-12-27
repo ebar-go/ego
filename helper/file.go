3b0ef36176a1b9310ed770d126b4cef71b383c73
hongker <xiaok2013@live.com>
2019-12-27 22:19:08 +0800

    优化文件路径判断方法

--- helper/file.go
+++ helper/file.go
@@ -1,21 +1,17 @@
 package helper
 
 import (
-	"fmt"
 	"os"
 )
 
 //IsPathExist 判断文件或文件夹是否存在
 func IsPathExist(path string) bool {
 	_, err := os.Stat(path)
-	if err != nil {
-		if os.IsExist(err) {
-			return true
-		}
-		if os.IsNotExist(err) {
-			return false
-		}
-		fmt.Println(err)
+	if err == nil {
+		return true
+	}
+
+	if os.IsNotExist(err) {
 		return false
 	}
 	return true
