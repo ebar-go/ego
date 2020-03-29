0ae229c444a22f4074e109ad25c48c5c65bd6f28
x <xiaok2013@live.com>
2020-03-29 18:58:22 +0800

    optimize response decode

---
+++ utils/curl/adapter.go
@@ -0,0 +1,48 @@
+package curl
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+)
+
+// Adapter buffer pool
+type Adapter struct {
+	pool sync.Pool
+}
+
+// NewAdapter
+func NewAdapter() *Adapter {
+	return &Adapter{pool:sync.Pool{New: func() interface{} {
+		return bytes.NewBuffer(make([]byte, 4096))
+	}}}
+}
+
+// StringifyResponse return response body as string
+func (adapter *Adapter) StringifyResponse(response *http.Response) (string, error) {
+	if response == nil {
+		return "", fmt.Errorf("response is empty")
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("response status code is:%d", response.StatusCode)
+	}
+
+	buffer := adapter.pool.Get().(*bytes.Buffer)
+	buffer.Reset()
+	defer func() {
+		if buffer != nil {
+			adapter.pool.Put(buffer)
+			buffer = nil
+		}
+	}()
+	_, err := io.Copy(buffer, response.Body)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to read respone:%s", err.Error())
+	}
+
+	return buffer.String(), nil
+}
--- utils/curl/curl.go
+++ utils/curl/curl.go
@@ -3,10 +3,11 @@ package curl
 import (
 	"errors"
 	"github.com/ebar-go/ego/app"
-	"io/ioutil"
 	"net/http"
 )
 
+// DefaultAdapter
+var DefaultAdapter = NewAdapter()
 
 // Execute return response as string
 func Execute(req *http.Request) (string, error) {
@@ -19,24 +20,10 @@ func Execute(req *http.Request) (string, error) {
 		return "", errors.New("no response")
 	}
 
-	return stringify(resp)
-}
-
-// stringify stringify http response, return string and error
-func stringify(resp *http.Response) (string, error) {
-	if resp.StatusCode != http.StatusOK {
-		return "", errors.New("response status code not 200")
-	}
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
 	// close response
 	defer func() {
 		_ = resp.Body.Close()
 	}()
 
-	return string(data), nil
+	return DefaultAdapter.StringifyResponse(resp)
 }
