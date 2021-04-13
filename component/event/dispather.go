420b992e487e461dad4577703d7bb4f356931422
hongker <774601526@qq.com>
2021-04-14 07:04:20 +0800

    event

--- component/event/dispather.go
+++ component/event/dispather.go
@@ -1,22 +1,32 @@
 package event
 
-var d = &dispatcher{items: map[string][]Listener{}}
+import "sync"
 
 // dispatcher
 type dispatcher struct {
 	items map[string][]Listener
+	rmw   sync.RWMutex
 }
+
+var (
+	// 初始化事件分发器，提前给map分配空间，减少因数组扩容带来的消耗
+	instance = &dispatcher{items: make(map[string][]Listener, 100), rmw: sync.RWMutex{}}
+)
 
 // Register
 func Register(eventName string, listener Listener) {
-	if Has(eventName) {
-		d.items[eventName] = append(d.items[eventName], listener)
-	} else {
-		d.items[eventName] = []Listener{listener}
+	instance.rmw.Lock()
+	defer instance.rmw.Unlock()
+	listeners, ok := instance.items[eventName]
+	if !ok {
+		// 预定义数组的长度为10
+		listeners = make([]Listener, 10)
 	}
+	listeners = append(listeners, listener)
+	instance.items[eventName] = listeners
 }
 
-// Listen
+// Listen register a sync event
 func Listen(eventName string, handler Handler) {
 	Register(eventName, Listener{
 		Mode:    Sync,
@@ -24,19 +34,24 @@ func Listen(eventName string, handler Handler) {
 	})
 }
 
-// Has
+// Has return event exist
 func Has(eventName string) bool {
-	_, ok := d.items[eventName]
+	instance.rmw.RLock()
+	defer instance.rmw.RUnlock()
+	_, ok := instance.items[eventName]
 	return ok
 }
 
 // Trigger
 func Trigger(eventName string, params interface{}) {
-	if !Has(eventName) {
+	instance.rmw.RLock()
+	defer instance.rmw.RUnlock()
+	listeners, ok := instance.items[eventName]
+	if !ok {
 		return
 	}
 
-	for _, listener := range d.items[eventName] {
+	for _, listener := range listeners {
 		if listener.Mode == Sync {
 			listener.Handler(Event{
 				Name:   eventName,
