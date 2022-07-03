ad9e71254648ad26d2e4dc444ff05366073bdf99
hongker <xiaok2013@live.com>
2022-07-03 10:50:20 +0800

    change paginator

--- http/pagination/pagination.go
+++ http/pagination/pagination.go
@@ -1,5 +1,12 @@
 package pagination
 
+import "math"
+
+const (
+	defaultCurrentPage = 1
+	defaultLimit = 10
+)
+
 // Paginator 分页器
 type Paginator struct {
 	// 总条数
@@ -13,8 +20,26 @@ type Paginator struct {
 }
 
 
+func (p *Paginator) SetTotalCount(total int) {
+	p.TotalCount = total
+	p.TotalPages = int(math.Ceil(float64(p.TotalCount) / float64(p.Limit)))
+}
+
 
 // GetOffset 获取偏移量
 func (p *Paginator) GetOffset() int {
 	return (p.CurrentPage - 1) * p.Limit
+}
+
+func New(currentPage, limit int) *Paginator {
+
+	if currentPage <= 0 {
+		currentPage = defaultCurrentPage
+	}
+
+	if limit <= 0 {
+		limit = defaultLimit
+	}
+
+	return &Paginator{CurrentPage: currentPage, Limit: limit}
 }
--- http/pagination/pagination_test.go
+++ http/pagination/pagination_test.go
@@ -5,15 +5,10 @@ import (
 	"testing"
 )
 
-// TestNewBuilder 测试分页
-func TestNewBuilder(t *testing.T) {
-	paginator := NewBuilder(100).Build()
-	assert.Equal(t, 1, paginator.CurrentPage)
-	assert.Equal(t, 10, paginator.Limit)
-	assert.Equal(t, 10, paginator.TotalPages)
-}
+
 
 func TestPaginator_GetOffset(t *testing.T) {
-	paginator := NewBuilder(20).SetCurrentPage(3).SetLimit(4).Build()
+	paginator := New(3, 4)
+	paginator.SetTotalCount(20)
 	assert.Equal(t, 8, paginator.GetOffset())
 }
