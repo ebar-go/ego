08e952b89b70af5672b2c04a9aa942e9ab368c3f
hongker <xiaok2013@live.com>
2022-08-20 21:51:37 +0800

    feat: 增加serializer接口的DecodeAndRelease方法

--- rebuild/utils/serializer/buffer.go
+++ rebuild/utils/serializer/buffer.go
@@ -6,26 +6,42 @@ import (
 	"github.com/ebar-go/ego/rebuild/utils/pool"
 )
 
+// Serializer provide bytes serialization to pointer
 type Serializer interface {
+	// Bytes returns the origin bytes data.
 	Bytes() []byte
+
+	// Decode serialize bytes to pointer value.
 	Decode(container interface{}) error
+
+	// Release put bytes to pool, avoiding memory leak and improve GC performance
 	Release()
+
+	// DecodeAndRelease execute Decode and Release function
+	DecodeAndRelease(container interface{}) error
 }
 
-type Buffer struct {
+// BufferSerializer implements Serializer interface by bytes.Buffer.
+type BufferSerializer struct {
 	*bytes.Buffer
 }
 
-func (buffer *Buffer) Decode(container interface{}) error {
+func (buffer *BufferSerializer) Decode(container interface{}) error {
 	return json.Unmarshal(buffer.Bytes(), container)
 }
 
-func (buffer *Buffer) Release() {
+func (buffer *BufferSerializer) DecodeAndRelease(container interface{}) error {
+	err := buffer.Decode(container)
+	buffer.Release()
+	return err
+}
+
+func (buffer *BufferSerializer) Release() {
 	pool.PutByte(buffer.Bytes())
 }
 
-func NewBuffer(length int) *Buffer {
-	return &Buffer{
+func NewBuffer(length int) *BufferSerializer {
+	return &BufferSerializer{
 		Buffer: bytes.NewBuffer(pool.GetByte(length)),
 	}
 }
