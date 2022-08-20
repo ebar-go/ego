7f5f0306597f785ced3bc275547337c505c1ca3b
hongker <xiaok2013@live.com>
2022-08-20 22:00:41 +0800

    feat: 增加buffer.Reset

--- rebuild/utils/serializer/buffer.go
+++ rebuild/utils/serializer/buffer.go
@@ -6,21 +6,6 @@ import (
 	"github.com/ebar-go/ego/rebuild/utils/pool"
 )
 
-// Serializer provide bytes serialization to pointer
-type Serializer interface {
-	// Bytes returns the origin bytes data.
-	Bytes() []byte
-
-	// Decode serialize bytes to pointer value.
-	Decode(container interface{}) error
-
-	// Release put bytes to pool, avoiding memory leak and improve GC performance
-	Release()
-
-	// DecodeAndRelease execute Decode and Release function
-	DecodeAndRelease(container interface{}) error
-}
-
 // BufferSerializer implements Serializer interface by bytes.Buffer.
 type BufferSerializer struct {
 	*bytes.Buffer
@@ -41,7 +26,9 @@ func (buffer *BufferSerializer) Release() {
 }
 
 func NewBuffer(length int) *BufferSerializer {
+	buffer := bytes.NewBuffer(pool.GetByte(length))
+	buffer.Reset()
 	return &BufferSerializer{
-		Buffer: bytes.NewBuffer(pool.GetByte(length)),
+		Buffer: buffer,
 	}
 }
---
+++ rebuild/utils/serializer/interface.go
@@ -0,0 +1,16 @@
+package serializer
+
+// Serializer provide bytes serialization to pointer
+type Serializer interface {
+	// Bytes returns the origin bytes data.
+	Bytes() []byte
+
+	// Decode serialize bytes to pointer value.
+	Decode(container interface{}) error
+
+	// Release put bytes to pool, avoiding memory leak and improve GC performance
+	Release()
+
+	// DecodeAndRelease execute Decode and Release function
+	DecodeAndRelease(container interface{}) error
+}
