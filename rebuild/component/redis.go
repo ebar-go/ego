fec12c4a67613a42ab63313bf0c782a9a866284d
hongker <xiaok2013@live.com>
2022-08-21 10:51:24 +0800

    增加redis组件

--- rebuild/component/redis.go
+++ rebuild/component/redis.go
@@ -1,6 +1,24 @@
 package component
 
-type Redis struct{}
+import (
+	"github.com/go-redis/redis"
+	"log"
+)
+
+type Redis struct {
+	redis.UniversalClient
+}
+
+func (r *Redis) Connect(options *redis.Options) error {
+	client := redis.NewClient(options)
+	_, err := client.Ping().Result()
+	if err != nil {
+		return err
+	}
+	r.UniversalClient = client
+	log.Println("Connect redis success:", options.Addr)
+	return nil
+}
 
 func NewRedis() *Redis {
 	return &Redis{}
