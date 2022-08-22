a1bd00bc49fa37c830da8572c602aec49f9bcd55
hongker <xiaok2013@live.com>
2022-08-22 12:07:46 +0800

    feat: 增加config读取配置文件的API

--- component/config.go
+++ component/config.go
@@ -7,6 +7,17 @@ type Config struct {
 	*viper.Viper
 }
 
+// LoadFile loads the configuration file specified.
+func (c *Config) LoadFile(paths ...string) error {
+	for _, p := range paths {
+		c.SetConfigFile(p)
+		if err := c.MergeInConfig(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func NewConfig() *Config {
 	return &Config{Named: componentConfig, Viper: viper.New()}
 }
