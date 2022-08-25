e0f59172161f624957c300e6e4a5a86425a9814e
hongker <xiaok2013@live.com>
2022-08-25 11:11:08 +0800

    feat: 增加组件示例

--- example/http_test.go
+++ example/http_test.go
@@ -2,12 +2,31 @@ package example
 
 import (
 	"github.com/ebar-go/ego"
+	"github.com/ebar-go/ego/component"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func TestAggregatorWithHttpServer(t *testing.T) {
+	aggregator := ego.NewAggregatorServer()
+
+	// 实例化一个http服务
+	httpServer := ego.NewHTTPServer(":8080").
+		RegisterRouteLoader(func(router *gin.Engine) { // 注册路由
+			router.GET("/", func(ctx *gin.Context) {
+				ctx.String(http.StatusOK, "home")
+			})
+		})
+
+	aggregator.WithServer(httpServer)
+
+	aggregator.Run()
+}
+
+func TestAggregatorWithHttpServerComplex(t *testing.T) {
 	aggregator := ego.NewAggregatorServer()
 
 	// 实例化一个http服务
@@ -23,6 +42,36 @@ func TestAggregatorWithHttpServer(t *testing.T) {
 		WithNotFoundHandler(func(ctx *gin.Context) { // 配置404
 			ctx.String(http.StatusNotFound, "404 Not Found")
 		}).
+		RegisterRouteLoader(func(router *gin.Engine) { // 注册路由
+			router.GET("/", func(ctx *gin.Context) {
+				ctx.String(http.StatusOK, "home")
+			})
+		})
+
+	aggregator.WithServer(httpServer)
+
+	aggregator.Run()
+}
+
+func TestAggregatorWithComponents(t *testing.T) {
+	aggregator := ego.NewAggregatorServer()
+
+	// component logger
+	component.Provider().Logger().Info("test logger info function")
+
+	// component cache
+	component.Provider().Cache().Default().Set("someCacheKey", "someCacheValue", time.Minute)
+
+	// component redis
+	if err := component.Provider().Redis().Connect(&redis.Options{
+		// ... some options like address, port
+	}); err != nil {
+		panic(err)
+	}
+	component.Provider().Redis().Set("someRedisKey", "someRedisVal", time.Minute)
+
+	// 实例化一个http服务
+	httpServer := ego.NewHTTPServer(":8080").
 		RegisterRouteLoader(func(router *gin.Engine) { // 注册路由
 			router.GET("/", func(ctx *gin.Context) {
 				ctx.String(http.StatusOK, "home")
