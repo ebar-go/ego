203b66ac17eea36e4482a9345581a294c1325c6e
hongker <xiaok2013@live.com>
2022-09-16 11:54:28 +0800

    feat: 设计Engine类

--- ego.go
+++ ego.go
@@ -1,23 +1,25 @@
 package ego
 
 import (
-	"github.com/ebar-go/ego/aggregator"
 	"github.com/ebar-go/ego/server"
 )
 
-// NewAggregator creates a new Aggregator instance
-func NewAggregatorServer() *aggregator.Aggregator {
-	return aggregator.NewAggregator()
+// New creates a new NamedEngine instance with default name.
+func New() *NamedEngine {
+	return NewNamedEngine("default")
 }
 
+// NewHttpServer creates a new http server instance.
 func NewHTTPServer(addr string) *server.HTTPServer {
 	return server.NewHTTPServer(addr)
 }
 
+// NewGrpcServer creates a new grpc server instance.
 func NewGRPCServer(addr string) *server.RPCServer {
 	return server.NewGRPCServer(addr)
 }
 
+// NewWebSocketServer creates a new web server instance.
 func NewWebsocketServer(addr string) *server.WebSocketServer {
 	return server.NewWebSocketServer(addr)
 }
---
+++ engine.go
@@ -0,0 +1,73 @@
+package ego
+
+import (
+	"github.com/ebar-go/ego/component"
+	"github.com/ebar-go/ego/runtime"
+	"github.com/ebar-go/ego/server"
+	"github.com/ebar-go/ego/utils/async"
+	"time"
+)
+
+// Engine includes the components and servers
+type Engine struct {
+	provider *component.Container
+	servers  []server.Server
+}
+
+// WithComponent use the provided components.
+func (engine *Engine) WithComponent(components ...component.Component) *Engine {
+	engine.provider.Register(components...)
+	return engine
+}
+
+// WithServer use the provided servers.
+func (engine *Engine) WithServer(servers ...server.Server) *Engine {
+	engine.servers = append(engine.servers, servers...)
+	return engine
+}
+
+func (engine *Engine) prepare() {
+	component.Initialize(engine.provider)
+}
+
+// Run runs the engine.
+func (engine *Engine) Run() {
+	engine.prepare()
+
+	runner := async.NewRunner()
+	for _, s := range engine.servers {
+		runner.Add(func(stop chan struct{}) {
+			s.Serve(stop)
+		})
+	}
+
+	runner.Start()
+
+	component.Provider().Logger().Info("Engine started successfully")
+
+	runtime.Shutdown(func() {
+		runner.Stop()
+		time.Sleep(time.Second)
+		component.Provider().Logger().Info("Engine stopped successfully")
+	})
+}
+
+// NamedEngine define engine with name.
+type NamedEngine struct {
+	*Engine
+	name string
+}
+
+// Run override engine Run function.
+func (engine *NamedEngine) Run() {
+	component.Provider().Logger().Infof("Running Engine:%s\n", engine.name)
+	engine.Engine.Run()
+}
+
+// NewNamedEngine creates a new named engine.
+func NewNamedEngine(name string) *NamedEngine {
+	return &NamedEngine{
+		name:   name,
+		Engine: &Engine{provider: component.NewContainer()},
+	}
+}
