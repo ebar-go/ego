a1d8ca4c9eb04c10910c4f064a7ffa40cc21cbbc
hongker <xiaok2013@live.com>
2022-11-23 22:25:54 +0800

    feat: add grpc example

--- examples/echo/server.go
+++ examples/echo/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/ebar-go/ego"
+	"github.com/ebar-go/ego/server"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,13 +10,16 @@ import (
 func main() {
 	aggregator := ego.New()
 
-	httpServer := ego.NewHTTPServer(":8080").EnableAvailableHealthCheck().
+	aggregator.WithServer(httpServer())
+
+	aggregator.Run()
+}
+
+func httpServer() server.Server {
+	return ego.NewHTTPServer(":8080").EnableAvailableHealthCheck().
 		WithDefaultRequestLogMiddleware().RegisterRouteLoader(func(router *gin.Engine) {
 		router.GET("echo", func(ctx *gin.Context) {
 			ctx.String(http.StatusOK, "Hello world")
 		})
 	})
-	aggregator.WithServer(httpServer)
-
-	aggregator.Run()
 }
