9464bf66c643846621902125c62e534909f9ebba
hongker <xiaok2013@live.com>
2022-11-24 00:30:19 +0800

    feat: add config component

--- provider.go
+++ provider.go
@@ -3,11 +3,13 @@ package ego
 import (
 	"github.com/ebar-go/ego/component"
 	"github.com/ebar-go/ego/component/cache"
+	"github.com/ebar-go/ego/component/config"
 	"sync"
 )
 
 type Provider struct {
-	cc component.Instance[*cache.Builder]
+	cb component.Instance[*cache.Builder]
+	cc component.Instance[*config.Config]
 }
 
 var providerInstance = struct {
@@ -18,12 +20,17 @@ var providerInstance = struct {
 func provider() *Provider {
 	providerInstance.once.Do(func() {
 		providerInstance.instance = &Provider{
-			cc: component.NewInstance[*cache.Builder]("default", cache.New()),
+			cb: component.NewInstance[*cache.Builder]("default", cache.New()),
+			cc: component.NewInstance[*config.Config]("default", config.New()),
 		}
 	})
 	return providerInstance.instance
 }
 
 func CacheBuilder() *cache.Builder {
+	return provider().cb.Delegate()
+}
+
+func Config() *config.Config {
 	return provider().cc.Delegate()
 }
