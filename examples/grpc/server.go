619dee697635aae4c28f4f37427591f0907f6b4f
hongker <xiaok2013@live.com>
2022-12-03 22:35:21 +0800

    feat: grpc example

--- examples/grpc/server.go
+++ examples/grpc/server.go
@@ -2,43 +2,67 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/ebar-go/ego"
 	"github.com/ebar-go/ego/component"
 	"github.com/ebar-go/ego/examples/grpc/pb"
 	"github.com/ebar-go/ego/utils/runtime"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
 	"google.golang.org/grpc/credentials/insecure"
-	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
 	aggregator := ego.New()
 
-	aggregator.Aggregate(httpServer(), grpcServer())
+	aggregator.Aggregate(httpServer())
+	aggregator.Aggregate(grpcServer())
 
 	aggregator.Run()
 }
 
+const (
+	target = "localhost:8081" // 与本地grpc服务建立连接，负载均衡无效
+	//target = "grpc-service:8081" // 与k8s的service建立连接，负载均衡无效
+	//target = "dns:///grpc-service:8081" // 与k8s的headless service建立连接，负载均衡有效
+)
+
 func httpServer() runtime.Runnable {
+	cc, err := grpc.Dial(target,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)),
+	)
+	client := pb.NewUserServiceClient(cc)
+
+	if err != nil {
+		panic(err)
+	}
+
 	return ego.NewHTTPServer(":8080").
 		EnableAvailableHealthCheck().
 		EnablePprofHandler().
 		WithDefaultRequestLogMiddleware().
 		RegisterRouteLoader(func(router *gin.Engine) {
 			router.GET("greeter", func(ctx *gin.Context) {
-				name := ctx.Query("name")
-				cc, err := grpc.Dial("127.0.0.1:8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
-				if err != nil {
-					component.Logger().Errorf("NewClient: %v", err)
-					return
+
+				num, _ := strconv.Atoi(ctx.Query("num"))
+				if num == 0 {
+					num = 10
 				}
-				resp, err := pb.NewUserServiceClient(cc).Greet(ctx, &pb.GreetRequest{Name: name})
-				if err != nil {
-					component.Logger().Errorf("Greet: %v", err)
-					return
+
+				for i := 0; i < num; i++ {
+					resp, err := client.Greet(ctx, &pb.GreetRequest{Name: "foo"})
+					if err != nil {
+						component.Logger().Errorf("Greet: %v", err)
+						return
+					}
+					component.Logger().Info(resp.Name)
 				}
-				ctx.String(http.StatusOK, resp.Name)
+
+				//ctx.String(http.StatusOK, resp.Name)
 			})
 		})
 }
@@ -48,7 +72,8 @@ type UserService struct {
 }
 
 func (srv UserService) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
-	return &pb.GreetResponse{Name: "greeter:" + in.GetName()}, nil
+	hostname, _ := os.Hostname()
+	return &pb.GreetResponse{Name: fmt.Sprintf("hostname=%s, in=%s, out=%s", hostname, in.Name, "bar")}, nil
 }
 
 func grpcServer() runtime.Runnable {
