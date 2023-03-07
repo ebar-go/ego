214e8f902c9fe3fb0b385c98f85a3c406ec801a2
hongker <xiaok2013@live.com>
2023-03-07 23:40:53 +0800

    benchmark timer

--- utils/timewheel/timer_test.go
+++ utils/timewheel/timer_test.go
@@ -16,3 +16,28 @@ func TestTimer(t *testing.T) {
 	})
 	time.Sleep(time.Second * 5)
 }
+
+func BenchmarkTimer(b *testing.B) {
+	// cpu is cost fast when create so much timer
+	// time-wheel timer is better
+	n := 400000
+	b.Run("timewheel", func(b *testing.B) {
+		b.ReportAllocs()
+		tw := New(time.Second, 64)
+		tw.Start()
+		defer tw.Stop()
+		for i := 0; i < n; i++ {
+			tw.AfterFunc(time.Second, func() {
+				_ = fmt.Sprintf("benchmark timer")
+			})
+		}
+	})
+	b.Run("normal", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < n; i++ {
+			time.AfterFunc(time.Second, func() {
+				_ = fmt.Sprintf("benchmark timer")
+			})
+		}
+	})
+}
